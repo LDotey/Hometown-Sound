462853298c33d9aa4b1de2868ba1e15d21eb5178
Leigh Dotey <leighdotey@gmail.com>
2025-01-09 12:16:40 -0500

    import flask-login & create instance of LoginManager, connect to app

--- server/config.py
+++ server/config.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -15,6 +16,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+# Used for encrypting session cookies
+app.secret_key = b'5:\xdd\x16\xf3\xd7\x8f\xfc\xeel\xbe\x84\x966\xaep'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
